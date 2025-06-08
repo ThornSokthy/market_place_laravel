7dfb8772964231eb0f5f540165b64aa8813ecc72
Thorn  Sokthy <sokthythorn@gmail.com>
2025-06-09 01:08:14 +0700

    Logout done

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -30,15 +30,60 @@
                 </div>
             @endguest
             @auth
-                <div class="flex items-center">
-                    <div class="flex items-center justify-center w-10 h-10 rounded-full overflow-hidden border-2 border-gray-200">
-                        <img
-                            class="w-full h-full object-cover"
-                            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRiZX50lN9UDKJALw9W8Lzes53T9GX8aKHIh15WJn4QBXx7ZjmAjsS5Yds&s"
-                            alt="Profile picture"
-                        />
+                <div class="flex items-center" x-data="{ open: false }" @click.away="open = false">
+                    <div class="relative flex items-center">
+                        <div
+                            class="group relative flex items-center justify-center w-10 h-10 rounded-full overflow-hidden border-2 border-gray-200 cursor-pointer hover:border-blue-500 transition-colors duration-200"
+                            @click="open = !open"
+                        >
+                            <img
+                                class="w-full h-full object-cover transition-transform duration-200 hover:scale-105"
+                                src="{{ Auth::user()->profile_photo_url ?? 'https://www.gravatar.com/avatar/' . md5(strtolower(trim(Auth::user()->email))) }}"
+                                alt="Profile picture"
+                            />
+                        </div>
+
+                        <span
+                            class="text-2xl ml-1 cursor-pointer hover:text-blue-500 transition-colors duration-200"
+                            @click="open = !open"
+                        >
+                            <i class='bx bx-chevron-down'></i>
+                        </span>
+
+                        <div
+                            class="absolute top-12 right-0 mt-1 w-48 bg-white rounded-md shadow-lg py-1 z-50 transition-all duration-200 ease-out origin-top-right"
+                            x-show="open"
+                            x-transition:enter="transition ease-out duration-200"
+                            x-transition:enter-start="opacity-0 scale-95"
+                            x-transition:enter-end="opacity-100 scale-100"
+                            x-transition:leave="transition ease-in duration-200"
+                            x-transition:leave-start="opacity-100 scale-100"
+                            x-transition:leave-end="opacity-0 scale-95"
+                            style="display: none"
+                        >
+                            <a
+                                href="#"
+                                class="block px-4 py-2 text-sm text-gray-700 hover:bg-blue-100/90 hover:text-blue-600 transition-all duration-200 ease-in-out"
+                            >
+                                <i class='bx bx-user mr-2'></i>My Profile
+                            </a>
+                            <a
+                                href="#"
+                                class="block px-4 py-2 text-sm text-gray-700 hover:bg-blue-100/90 hover:text-blue-600 transition-all duration-200 ease-in-out"
+                            >
+                                <i class='bx bx-cog mr-2'></i>Settings
+                            </a>
+                            <form method="POST" action="{{ route('logout') }}">
+                                @csrf
+                                <button
+                                    type="submit"
+                                    class="block text-start w-full cursor-pointer px-4 py-2 text-sm text-red-600 hover:bg-red-100/90 hover:text-red-700 transition-all duration-200 ease-in-out"
+                                >
+                                    <i class='bx  bx-arrow-out-left-square-half mr-2' ></i>Logout
+                                </button>
+                            </form>
+                        </div>
                     </div>
-                    <span class="text-2xl ml-1"><i class='bx bx-chevron-down'  ></i> </span>
                 </div>
             @endauth
         </div>
@@ -59,7 +104,7 @@
             <span class="flex justify-between gap-4">
                 <span class="text-3xl"><i class='bx bx-heart'></i> </span>
                 <span class="text-3xl"><i class='bx bx-cart'></i> </span>
-            </div>
+            </span>
         </div>
     </header>
 
--- routes/web.php
+++ routes/web.php
@@ -10,3 +10,4 @@ Route::view('/register', 'auth.register')->name('register');
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
+Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
